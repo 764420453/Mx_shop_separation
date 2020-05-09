76f36845eb7b8554182d8fcab2c278eea59a6930
浮生未歇 <764420453@qq.com>
2020-05-09 09:35:36 +0800

    交易管理模型的后台配置

---
+++ MxShop/apps/trade/adminx.py
@@ -0,0 +1,28 @@
+# trade/adminx.py
+__author__ = 'derek'
+
+import xadmin
+from .models import ShoppingCart, OrderInfo, OrderGoods
+
+
+class ShoppingCartAdmin(object):
+    list_display = ["user", "goods", "nums", ]
+
+
+xadmin.site.register(ShoppingCart, ShoppingCartAdmin)
+
+
+class OrderInfoAdmin(object):
+    list_display = ["user", "order_sn", "trade_no", "pay_status", "post_script", "order_mount",
+                    "order_mount", "pay_time", "add_time"]
+
+    class OrderGoodsInline(object):
+        model = OrderGoods
+        exclude = ['add_time', ]
+        extra = 1
+        style = 'tab'
+
+    inlines = [OrderGoodsInline, ]
+
+
+xadmin.site.register(OrderInfo, OrderInfoAdmin)
